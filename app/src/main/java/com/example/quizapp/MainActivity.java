844aa2a37132e8bfeb7e7d59c24cb7bf5c2fb961
Nesar Ahmed <nesar13@hotmail.com>
2019-12-23 02:01:32 -0500

    added onClickListener for buttons

--- app/src/main/java/com/example/quizapp/MainActivity.java
+++ app/src/main/java/com/example/quizapp/MainActivity.java
@@ -3,12 +3,42 @@ package com.example.quizapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button trueButton;
+    private  Button falseButton;
+    private TextView questionText;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        falseButton=findViewById(R.id.false_button);
+        trueButton= findViewById(R.id.true_button);
+        questionText=findViewById(R.id.answer_text_view);
+
+        falseButton.setOnClickListener(this);
+        trueButton.setOnClickListener(this);
+
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.false_button:
+                Toast.makeText(MainActivity.this, "False",
+                        Toast.LENGTH_SHORT);
+
+
+        }
+
     }
 }
