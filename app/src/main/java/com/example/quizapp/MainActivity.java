b318dbb39703c82c62a5997d230cbcb9cce27054
Nesar Ahmed <nesar13@hotmail.com>
2019-12-24 17:29:09 -0500

    Update MainActivity.java

--- app/src/main/java/com/example/quizapp/MainActivity.java
+++ app/src/main/java/com/example/quizapp/MainActivity.java
@@ -11,12 +11,12 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Button trueButton;
-    private  Button falseButton;
+    private Button falseButton;
     private TextView questionText;
     private Button nextButton;
-    private int currentIndex=0;
+    private int currentIndex = 0;
 
-    private Question[] questionBank=new Question[] {
+    private Question[] questionBank = new Question[]{
             new Question(R.string.perpendicular_question, false),
             new Question(R.string.product_question, false),
             new Question(R.string.slope_question, true),
@@ -26,16 +26,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        falseButton=findViewById(R.id.false_button);
-        trueButton= findViewById(R.id.true_button);
-        questionText=findViewById(R.id.answer_text_view);
-        nextButton=findViewById(R.id.next_button);
+        falseButton = findViewById(R.id.false_button);
+        trueButton = findViewById(R.id.true_button);
+        questionText = findViewById(R.id.answer_text_view);
+        nextButton = findViewById(R.id.next_button);
 
 
         falseButton.setOnClickListener(this); //using the interface View.onClickListener Ctrl+B
         trueButton.setOnClickListener(this);
         nextButton.setOnClickListener(this);
-
 
 
     }
@@ -45,24 +44,41 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (v.getId()) {
             case R.id.false_button:
                 //context another way of setting context(Mainactivity.this, and getApplicationContext())
-                Toast.makeText(MainActivity.this, "False",
-                        Toast.LENGTH_SHORT).show();
+//                Toast.makeText(MainActivity.this, "False",
+//                        Toast.LENGTH_SHORT).show();
+
+                isAnswerCorrect(false);
                 break;
             case R.id.true_button:
-                Toast.makeText(MainActivity.this,
-                        "True", Toast.LENGTH_SHORT).show();
+                
+                isAnswerCorrect(true);
                 break;
+
             case R.id.next_button:
                 //on to the next question
 
-                currentIndex=(currentIndex+1)%questionBank.length;
+                currentIndex = (currentIndex + 1) % questionBank.length;
                 nextButtonUpdate();
 
         }
 
     }
-    public void nextButtonUpdate(){
-        Log.d("Current", "onClick: "+currentIndex);
+
+    public void nextButtonUpdate() {
+        Log.d("Current", "onClick: " + currentIndex);
         questionText.setText(questionBank[currentIndex].getAnswerResID());
+    }
+
+    public void isAnswerCorrect(boolean answer) {
+        boolean temp = questionBank[currentIndex].isAnswerTrue();
+        int toastID = 0;
+
+        if (answer == temp) {
+            toastID = R.string.correct_answer;
+        } else {
+            toastID = R.string.incorrect_answer;
+        }
+        Toast.makeText(MainActivity.this, toastID, Toast.LENGTH_SHORT).show();
+
     }
 }
