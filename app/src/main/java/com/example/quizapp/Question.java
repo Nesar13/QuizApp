110a4fafa7513adf2eb4d04354f00ce2370ac031
Nesar Ahmed <nesar13@hotmail.com>
2019-12-23 20:03:38 -0500

    model Question.java creation

---
+++ app/src/main/java/com/example/quizapp/Question.java
@@ -0,0 +1,24 @@
+package com.example.quizapp;
+
+public class Question {
+
+    private int answerResID;
+    private boolean answerTrue;
+
+
+    public int getAnswerResID() {
+        return answerResID;
+    }
+
+    public void setAnswerResID(int answerResID) {
+        this.answerResID = answerResID;
+    }
+
+    public boolean isAnswerTrue() {
+        return answerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        this.answerTrue = answerTrue;
+    }
+}
