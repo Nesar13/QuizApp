0915e587f40e8174a3f95b370f8d404782f1be3f
Nesar Ahmed <nesar13@hotmail.com>
2019-12-23 20:14:27 -0500

    Added questions

--- app/src/main/java/com/example/quizapp/Question.java
+++ app/src/main/java/com/example/quizapp/Question.java
@@ -5,6 +5,10 @@ public class Question {
     private int answerResID;
     private boolean answerTrue;
 
+    public Question(int answerResID, boolean answerTrue) {
+        this.answerResID = answerResID;
+        this.answerTrue = answerTrue;
+    }
 
     public int getAnswerResID() {
         return answerResID;
